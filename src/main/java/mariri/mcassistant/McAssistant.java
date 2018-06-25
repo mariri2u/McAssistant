99893c765dbb4cac3ae2eeb9afc09a856c747d69
mariri <fily.maruri@gmail.com>
2018-06-26 01:01:33 +0900

    Ver.1.12.2-0.1-alphaを公開

--- src/main/java/mariri/mcassistant/McAssistant.java
+++ src/main/java/mariri/mcassistant/McAssistant.java
@@ -37,7 +37,7 @@ public class McAssistant {
 
         public static final String MODID = "mcassistant";
         public static final String NAME = "McAssistant";
-        public static final String VERSION = "1.12.2-1.0";
+        public static final String VERSION = "1.12.2-0.1-alpha";
         public static final String CONFIG_LANG = "mcassistant.config";
         public static Configuration CONFIG;
 
